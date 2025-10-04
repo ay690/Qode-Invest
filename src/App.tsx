7f121f38094b586bb64ec619cb68e418d1c8bfd8
Aniket Yadav <aniketyadav690@gmail.com>
2025-10-04 20:33:05 +0530

    initilaized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="text-sm text-center font-extrabold">Hello!!!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
