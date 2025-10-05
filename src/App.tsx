8588ea20950529f8b5ccd2f7cf8c73af627c9df0
Aniket Yadav <aniketyadav690@gmail.com>
2025-10-05 12:52:58 +0530

    fix: build errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Layout } from "./components/layout";
+import { Layout } from "./components/Layout";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
